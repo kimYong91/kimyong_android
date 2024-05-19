9ccb8a62362391879d31890d5b05f6f95ebc5fe2
rladyd <rladyd97@naver.com>
2024-05-19 20:47:49 +0900

    2024.05.19 (loginSwitch 문제 있음)

--- app/src/main/java/com/example/kimyong_android/SecondActivity.kt
+++ app/src/main/java/com/example/kimyong_android/SecondActivity.kt
@@ -2,30 +2,22 @@ package com.example.kimyong_android
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.MenuItem
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.kimyong_android.databinding.ActivityMainBinding
+import com.example.kimyong_android.databinding.ActivitySecondBinding
 
-class MainActivity : AppCompatActivity() {
+class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityMainBinding.inflate(layoutInflater)
+        val binding = ActivitySecondBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.loginButton.setOnClickListener {
-            val intent = Intent(this, SecondActivity::class.java)
+        binding.saveButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
-
-        if (binding.loginSwitch.isEnabled) {
-            Toast.makeText(this, "로그인 유지", Toast.LENGTH_LONG).show()
-        } else {
-            Toast.makeText(this, "로그인 유지 해제", Toast.LENGTH_SHORT).show()
-        }
-
     }
 }
