0a411815c24f58565b0fcb55cfabc0a5d7c1d50c
rladyd <rladyd97@naver.com>
2024-05-24 01:37:13 +0900

    2024.05.24

--- ch09_recycler_view/src/main/java/com/busanit/ch09_recycler_view/PlusDeleteActivity.kt
+++ ch09_recycler_view/src/main/java/com/busanit/ch09_recycler_view/PlusDeleteActivity.kt
@@ -3,10 +3,7 @@ package com.busanit.ch09_recycler_view
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
